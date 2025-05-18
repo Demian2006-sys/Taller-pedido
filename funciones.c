636f16fe7fe9cf1973dadb6913f34136953acdc8
Demian2006-sys <demian.durand@udla.edu.ec>
2025-05-18 16:31:56 -0500

    Add files via upload

---
+++ funciones.c
@@ -0,0 +1,236 @@
+#include <stdio.h>
+#include <string.h>
+#include "funciones.h"
+
+int menu() {
+    int opc;
+    printf("\nSeleccione una opción\n");
+    printf("1. Ingreso de componentes/material\n");
+    printf("2. Ingreso de productos\n");
+    printf("3. Realizar pedidos\n");
+    printf("4. Editar/Borrar Producto\n");
+    printf("5. Editar/Borrar componentes\n");
+    printf("6. Salir\n");
+    printf(">> ");
+    opc = validar_cantidad(opc);
+    while (getchar() != '\n'); // limpiar el buffer
+    return opc;
+}
+
+void ing_recursos(int *contcomp, char component[8][50], int *confirm, int cant[5][8], int cantidad[8]) {
+    if (*contcomp >= 8) {
+        printf("Límite de componentes alcanzado.\n");
+        return;
+    }
+
+    printf("Ingrese el nombre del nuevo componente: ");
+    fgets(component[*contcomp], 50, stdin);
+    component[*contcomp][strcspn(component[*contcomp], "\n")] = '\0'; // eliminar salto de línea
+
+    printf("Ingrese la cantidad de %s: ", component[*contcomp]);
+    cantidad[*contcomp] = validar_cantidad(cantidad[*contcomp]);
+    while (getchar() != '\n'); // limpiar buffer
+
+    (*contcomp)++;
+    *confirm++;
+}
+
+void ing_Productos(int *contprod, int contcomp, char producto[5][50], char component[8][50], int cant[5][8], float tiempo[5], int confirm2) {
+    if (*contprod >= 5) {
+        printf("Límite de productos alcanzado (5).\n");
+        return;
+    }
+
+    printf("Ingrese el nombre del producto %d: ", *contprod + 1);
+    fgets(producto[*contprod], 50, stdin);
+    producto[*contprod][strcspn(producto[*contprod], "\n")] = '\0'; // eliminar salto de línea
+
+    printf("Ingrese la cantidad necesaria de cada componente para producir un solo %s:\n", producto[*contprod]);
+    for (int i = 0; i < contcomp; i++) {
+        printf("%s: ", component[i]);
+        cant[*contprod][i] = validar_cantidad(cant[*contprod][i]);
+    }
+
+    printf("Ingrese el tiempo necesario para producir un %s (en minutos): ", producto[*contprod]);
+    tiempo[*contprod] = validar_cantidad(tiempo[*contprod]);
+    while (getchar() != '\n');
+
+    (*contprod)++;
+     confirm2++;
+}
+
+void pedido(int *contprod, char producto[5][50], int cant[5][8], float tiempo[5], char component[8][50], int cantidad[8]) {
+    int opclist;
+    float tiempo_total;
+
+    if (*contprod == 0) {
+        printf("No hay productos registrados para realizar un pedido.\n");
+        return;
+    }
+
+    printf("\nSeleccione el producto que desea pedir:\n\n");
+    printf("%-5s | %-25s\n", "N°", "Producto");
+    printf("------------------------------------------\n");
+
+    for (int i = 0; i < *contprod; i++) {
+        printf("%-5d | %-25s\n", i, producto[i]);
+    }
+
+    do {
+        printf("\nIngrese el número del producto: ");
+        opclist = validar_cantidad(opclist);
+
+        if (opclist < 0 || opclist >= *contprod) {
+            printf("Opcion invalida, por favor seleccione un numero valido de la lista.\n");
+        }
+    } while (opclist < 0 || opclist >= *contprod);
+
+    printf("¿Cuantos %s desea pedir? ", producto[opclist]);
+    int cantidad_pedida;  // <- nuevo nombre para evitar conflicto
+    cantidad_pedida = validar_cantidad(cantidad_pedida);
+
+    // Verificación de recursos
+    for (int i = 0; i < *contprod; i++) {
+        int necesario = cant[opclist][i] * cantidad_pedida;
+        if (necesario > cantidad[i]) {
+            printf("No hay suficiente del recurso '%s'. Necesario: %d, Disponible: %d\n", component[i], necesario, cantidad[i]);
+            printf("No se puede cumplir el pedido por falta de recursos.\n");
+            return;
+        }
+    }
+
+    printf("¿En qué tiempo desea recibir el pedido el cliente? (en minutos): ");
+    float tiempo_pedido;
+    tiempo_pedido = validar_cantidad(tiempo_pedido);
+    tiempo_total = tiempo[opclist] * cantidad_pedida;
+    float falta;
+    falta= tiempo_total - tiempo_pedido;
+
+    if (tiempo_total > tiempo_pedido) {
+        printf("No se puede cumplir el pedido en el tiempo solicitado, faltan %.2f minutos.\n", falta);
+        return;
+    }
+    // Descontar recursos usados
+    for (int i = 0; i < 8; i++) {
+        cantidad[i] -= cant[opclist][i] * cantidad_pedida;
+    }
+
+    printf("Pedido de %d %s confirmado. Tiempo estimado de entrega: %.2f minutos.\n", cantidad_pedida, producto[opclist], tiempo_total);
+}
+
+void edit_prod(int *contprod, char producto[5][50], int cantstck[5], char component[8][50], int cant[5][8], float tiempo[5], int contcomp, int confirm2 ) {
+    int pos, opc;
+    if (*contprod == 0) {
+        printf("No hay productos registrados.\n");
+        return;
+    }
+
+    printf("Productos:\n");
+    for (int i = 0; i < *contprod; i++) {
+        printf("%d. %s\n", i + 1, producto[i]);
+    }
+
+    printf("Seleccione un producto: ");
+    pos = validar_cantidad(pos);
+    pos--;
+
+    if (pos < 0 || pos >= *contprod) {
+        printf("Opción inválida.\n");
+        return;
+    }
+
+    printf("1. Editar producto\n2. Eliminar producto\n>> ");
+    opc = validar_cantidad(opc);
+    while (getchar() != '\n');
+
+    if (opc == 1) {
+        printf("Nuevo nombre: ");
+        fgets(producto[pos], 50, stdin);
+        producto[pos][strcspn(producto[pos], "\n")] = '\0';
+
+        printf("Nueva cantidad de cada componente:\n");
+        for (int i = 0; i < contcomp; i++) {
+            printf("%s: ", component[i]);
+            cant[pos][i] = validar_cantidad(cant[pos][i]);
+        }
+
+        printf("Nuevo tiempo de producción (en minutos): ");
+        tiempo[pos] = validar_cantidad(tiempo[pos]);
+        while (getchar() != '\n');
+
+        printf("Producto editado con éxito.\n");
+    } else if (opc == 2) {
+        for (int i = pos; i < *contprod - 1; i++) {
+            strcpy(producto[i], producto[i + 1]);
+            for (int j = 0; j < 8; j++) {
+                cant[i][j] = cant[i + 1][j];
+            }
+            tiempo[i] = tiempo[i + 1];
+        }
+        (*contprod)--;
+        confirm2--;
+        printf("Producto eliminado.\n");
+    } else {
+        printf("Opción inválida.\n");
+    }
+}
+
+void edit_recursos(int *contcomp, char component[8][50], int cant[5][8], int cantidad[8], int confirm) {
+    int pos, opc;
+    if (*contcomp == 0) {
+        printf("No hay componentes registrados.\n");
+        return;
+    }
+
+    printf("Componentes:\n");
+    for (int i = 0; i < *contcomp; i++) {
+        printf("%d. %s\n", i + 1, component[i]);
+    }
+
+    printf("Seleccione un componente: ");
+    scanf("%d", &pos);
+    pos--;
+
+    if (pos < 0 || pos >= *contcomp) {
+        printf("Opción inválida.\n");
+        return;
+    }
+
+    printf("1. Editar componente\n2. Eliminar componente\n>> ");
+    scanf("%d", &opc);
+    while (getchar() != '\n');
+
+    if (opc == 1) {
+        printf("Nuevo nombre: ");
+        fgets(component[pos], 50, stdin);
+        component[pos][strcspn(component[pos], "\n")] = '\0';
+        printf("Componente editado.\n");
+        printf("Nueva cantidad: ");
+
+        cantidad[pos] = validar_cantidad(cantidad[pos]);
+
+        printf("Cantidad editada.\n");
+    } else if (opc == 2) {
+        for (int i = pos; i < *contcomp - 1; i++) {
+            strcpy(component[i], component[i + 1]);
+        }
+        (*contcomp)--;
+        confirm--;
+        printf("Componente eliminado.\n");
+    } else {
+        printf("Opcion invalida.\n");
+    }
+}
+
+int validar_cantidad(int num)
+{
+    int validacion;
+    validacion = scanf("%d", &num);
+    while (validacion != 1 || num<=0)
+    {
+        printf("Entrada invalida. Por favor, ingrese un numero entero positivo: ");
+        while (getchar() != '\n');
+        validacion = scanf("%d", &num);
+    }
+    return num;
+}
---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include "funciones.h"
+int main(int argc, char *argv[]) {
+    int contprod = 0;
+    int contcomp = 0;
+    int confirm = 0;
+    int confirm2 = 0;
+    char producto[5][50];
+    char component[8][50];
+    int cant[5][8];
+    float tiempo[5];
+    int cantstck[5];
+    int cantidad[8];
+    int opc;
+
+    do {
+        opc = menu();
+        switch (opc) {
+            case 1:
+                ing_recursos(&contcomp, component, &confirm, cant, cantidad);
+                break;
+            case 2:
+                ing_Productos(&contprod, contcomp, producto, component, cant, tiempo,confirm2);
+                break;
+            case 3:
+                pedido(&contprod, producto, cant, tiempo, component, cantidad);
+                break;
+            case 4:
+                edit_prod(&contprod, producto, cantstck, component, cant, tiempo, contcomp, confirm2);
+                break;
+            case 5:
+                edit_recursos(&contcomp, component, cant, cantidad, confirm);
+                break;
+            case 6:
+                printf("Saliendo del programa.\n");
+                break;
+            default:
+                printf("Opción no válida. Intente de nuevo.\n");
+                break;
+        }
+    } while (opc != 6);
+
+    return 0;
+}
